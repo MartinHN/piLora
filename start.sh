dee4789928cca5554dfc1b6e5b16874402a0808a
tinmar <martin.hermant@gmail.com>
2023-08-14 15:47:10 +0200

    fix start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 mount | grep 'type ext4' | grep rw
 isRW=$?
 echo "booted in ${isRW} (rw:0, ro:1)"
-if [[ $isRW != 0 ]]; then sudo mount -o remount,rw /; fi
+if [[ $isRW != 0 ]]; then
+    sudo mount -o remount,rw /
+fi
+echo "starting e32"
 sudo systemctl start e32
-if [[ $isRW != 0 ]]; then sudo mount -o remount,ro /; fi
-sleep 1
+echo "e32 started"
+if [[ $isRW != 0 ]]; then
+    sudo mount -o remount,ro /
+fi
+
+# sleep 1
+echo "e32ws started"
 ./e32ws
